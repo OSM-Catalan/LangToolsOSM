b7bb67a2af6cd1af39602abdec3fba5157e52aaf
Joan Maspons <joanmaspons@gmail.com>
2021-11-20 16:43:07 +0100

    Improve output

--- translate_with_wikidata/cli.py
+++ translate_with_wikidata/cli.py
@@ -51,7 +51,7 @@ def write_db(db, file, format='csv', table_name=None):
                 writer.headers = headers
                 writer.table_name = table_name
                 matrix = []
-                pattern = re.compile(r'((node|way|relation)\|[0-9]+)')
+                pattern = re.compile(r'((node|way|relation)\|[0-9]+),')
                 for wikidata, values in db.items():
                     row = db_item_row(wikidata, values)
                     row[0] = f'[https://www.wikidata.org/wiki/{row[0]} {row[0]}]'
@@ -83,26 +83,25 @@ def db_item_row(db_key, db_item):
 @click.option('--area', prompt='Bounding box (South,West,North,East), overpass filters or the exact name value of an area', type=str, help='Search area (eg. "42.49,2.43,42.52,2.49", "[name_int=Kobane]" or "Le Canigou").')
 @click.option('--cache-answers', default=False, is_flag=True, help='Remember the answers for objects with the same wikidata value.')
 @click.option('--dry-run', default=False, is_flag=True, help='Run the program without saving any change to OSM. Useful for testing. No login required.')
-@click.option('--filters', type=str, help="""Overpass filters to search for objects. Default to "nwr['name'][~'name:[a-z]+'~'.']['wikidata'][!'name:{lang}']""""")
+@click.option('--filters', type=str, help="""Overpass filters to search for objects. Default to "nwr['name'][~'name:[a-z]+'~'.']['wikidata'][!'name:{lang}']".""")
 @click.option('--lang', prompt='Language to add a multilingual name key (e.g. ca, en, ...)', type=str, help='A language ISO 639-1 Code. See https://wiki.openstreetmap.org/wiki/Multilingual_names .')
 @click.option('--output', type=click.Path(dir_okay=False, writable=True), help='Path of the file to write db of wikidata translations and user answers.')
-@click.option('--output-format', type=click.Choice(['csv', 'mediawiki'], case_sensitive=False), help='Format of the output file.')
+@click.option('--output-format', type=click.Choice(['csv', 'mediawiki'], case_sensitive=False), default='csv', help='Format of the output file.')
 @click.option('--username', type=str, help='OSM user name to login and commit changes. Ignored in --dry-run mode.')
-@click.option('--verbose', '-v', count=True, help='Print the changeset tags and all the tags of the features that you are currently editing.')
+@click.option('--verbose', '-v', count=True, help='Print all the tags of the features that you are currently editing.')
 def translate_with_wikidatacommand(area, dry_run, cache_answers, filters, lang, output, output_format, username, verbose):
     """Add «name:LANG» selecting the label or alias from «wikidata»."""
     if not dry_run:
         api = lt.login_OSM(username=username)
-    changeset_tags = {u"comment": f"Fill empty name:{lang} tags translations from wikidata in {area} for {filters}",
-                      u"source": u"wikidata", u"created_by": f"LangToolsOSM {__version__}"}
-    if verbose:
-        print(changeset_tags)
-
     if not filters:
         filters = f"nwr['name'][~'name:[a-z]+'~'.']['wikidata'][!'name:{lang}']"
     result = lt.get_overpass_result(area=area, filters=filters)
-    if verbose > 1:
-        print(str(len(result.nodes)) + ' nodes; ' + str(len(result.ways)) + 'ways; ' + str(len(result.relations)) + ' relations')
+    print(str(len(result.nodes)) + ' nodes ' + str(len(result.ways)) + 'ways; ' + str(len(result.relations)) + ' relations found.')
+    print()
+    print('After the firs edition a changeset with the following tags:')
+    changeset_tags = {u"comment": f"Fill empty name:{lang} tags translations from wikidata in {area} for {filters}",
+                      u"source": u"wikidata", u"created_by": f"LangToolsOSM {__version__}"}
+    print(changeset_tags)
 
     changeset = None
     db = dict()
@@ -122,22 +121,31 @@ def translate_with_wikidatacommand(area, dry_run, cache_answers, filters, lang, 
                 if output:
                     db[translations['id']]['objects'].append({'name': rn.tags['name'], 'type': rn._type_value,
                                                               'id': rn.id, 'modified': False})
-            if verbose > 1:
+            if verbose > 2:
                 print(translations['translations'])
+            if not dry_run:
+                if n_edits < 195:
+                    print(f'Number of editions in the current changeset: {n_edits}')
+                elif n_edits < 200:
+                    print(Fore.YELLOW + f'Number of editions in the current changeset: {n_edits} (> 200 is considered a mass modification)' + Style.RESET_ALL)
+                else:
+                    print(Fore.RED + f'Too much editions in the current changeset: {n_edits}(> 200 is considered a mass modification)')
+                    print('Press "Ctrl-c" to STOP now.' + Style.RESET_ALL)
+                if verbose > 1 and changeset:
+                    print(Fore.CYAN + f'Changeset opened: https://www.osm.org/changeset/{changeset}' + Style.RESET_ALL)
+            lt.print_element(rn, verbose=verbose)
             tags = {}
 
             if cache_answers and db[translations['id']]['answer']['committed']:
+                print(Fore.BLUE + 'Remembering your response...' + Style.RESET_ALL)
                 tags["name:" + lang] = db[translations['id']]['answer']['value']
             else:
                 if (
                         cache_answers and db[translations['id']]['answer']['committed'] is None and
                         db[translations['id']]['answer']['value'] == '-'
                 ):
+                    print(Fore.BLUE + 'Remembering your response... SKIP.' + Style.RESET_ALL)
                     continue
-
-                if not dry_run:
-                    print(f'Number of editions in the current changeset: {n_edits}')
-                lt.print_element(rn, verbose=verbose)
 
                 if translations['translations']:
                     if translations['translations'] and translations['translations']['label']['value']:
@@ -145,31 +153,33 @@ def translate_with_wikidatacommand(area, dry_run, cache_answers, filters, lang, 
                         translation_options = [translations['translations']['label']['value']]
                     else:
                         translation_options = []
+
                     if translations['translations']['aliases']:
                         i = 1
                         for alias in translations['translations']['aliases']:
                             print(str(i) + ' = ' + alias['value'])
                             translation_options.append(alias['value'])
                             i = i + 1
-                    if verbose > 1:
+
+                    if verbose > 2:
                         print('translation_options: ' + str(translation_options))
 
                     if translation_options:
                         select_translation = input("Select translation ('-' to skip): ") or '0'
-                    else:
-                        select_translation = '-'
-                        print(Fore.YELLOW + "SKIP: WRONG path not translations['translations']? No translations from wikidata." + Style.RESET_ALL)
+                        while select_translation not in [str(x) for x in range(len(translation_options))] + ['-']:
+                            print('Enter a number from 0 to ' + str(len(translation_options) - 1))
+                            select_translation = input("Select translation ('-' to skip): ") or '0'
 
-                    while select_translation not in [str(x) for x in range(len(translation_options))] + ['-']:
-                        print('Enter a number from 0 to ' + str(len(translation_options) - 1))
-                        select_translation = input("Select translation ('-' to skip): ") or '0'
                 else:
                     select_translation = '-'
-                    print(Fore.RED + 'SKIP: No translations from wikidata.' + Style.RESET_ALL)
 
                 if select_translation in '-':
                     db[translations['id']]['answer']['value'] = '-'
                     db[translations['id']]['answer']['committed'] = None
+                    if translations['translations']:
+                        print(Fore.BLUE + 'SKIP.' + Style.RESET_ALL)
+                    else:
+                        print(Fore.BLUE + 'SKIP: No translations from wikidata.' + Style.RESET_ALL)
                     continue
                 else:
                     select_translation = int(select_translation)
